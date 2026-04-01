220a56df13e953798201bdd85695aa14b35de2e0
skyacinthus <salmakarimahkamal@gmail.com>
2026-04-01 20:43:20 +0700

    fix: bat

--- compile_win.bat
+++ compile_win.bat
@@ -1,7 +1,10 @@
 @echo off
 echo Compiling for Windows...
-gcc -c src/sqlite3.c -o sqlite3.o -Os
-g++ -std=c++11 src/*.cpp sqlite3.o -o LabReservation.exe -static
-del sqlite3.o
+
+gcc -c src/sqlite3.c -o sqlite3.o -Iinclude -Os
+
+g++ src/*.cpp sqlite3.o -Iinclude -o LabReservation.exe -static -std=c++11 -pthread
+
+del *.o
 echo Done! Run LabReservation.exe
 pause
